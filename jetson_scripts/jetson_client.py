377f1c4e25996b4ff63f69c5882a9e7d149d0eb4
Grzetan <grzetan@gmail.com>
2023-12-21 08:47:19 +0100

    Started implementing jetson client script

--- jetson_scripts/jetson_client.py
+++ jetson_scripts/jetson_client.py
@@ -1,71 +1,47 @@
 import cv2
 import os
 import time
+import subprocess
+import base64
+from io import BytesIO
+import requests
 
-cap = cv2.VideoCapture(0)
+img = cv2.imread("ok.png")
 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+start = time.time()
+_, encoded = cv2.imencode(".png", img)
+base64_image = base64.b64encode(encoded.tobytes()).decode('utf-8')
+cv2.imwrite("/tmp/detection.png", img)
 
-photodir = '/home/hf/Nagrania'
+data = {"timestamp": 12345, "latitude": 90, "longitude": 12, "image": base64_image}
 
-if not os.path.exists(photodir):
-    os.makedirs(photodir)
+res = requests.post("http://127.0.0.1:3000/detection", json=data)
+print(res)
 
-writer = None
+out = subprocess.check_output(f"base64 /tmp/detection.png | curl -d @- \"http://127.0.0.1:9001/pbl-2023/2?api_key=kY5y1URLQQGG8D9Fp5LP\"", shell=True, text=True)
+end = time.time()
+print(end - start)
 
-save = False
+print(out)
+# cap = cv2.VideoCapture(0)
 
-print("Press q to start recording and press q once again to stop. Press space to save a photo")
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-j = 0
-interval = 60 # Interval to save video (in seconds)
-record_duration =  60 # Duration to record video (in seconds)
-start_time = time.time()
+# while True:
+#     ret, frame = cap.read()
 
-while True:
-    ret, frame = cap.read()
+#     cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
+#     cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+#     cv2.imshow('frame', frame)
 
-    if save:
-        if writer is None:
-            while os.path.exists(os.path.join(photodir, f'video{j}.mp4')):
-                j += 1
-            writer = cv2.VideoWriter(os.path.join(photodir, f'video{j}.mp4'), cv2.VideoWriter_fourcc(*"mp4v"), 9, (width, height))
+#     key = cv2.waitKey(1)
+#     if key == 27:
+#         break
 
-        writer.write(frame)
-        current_time = time.time()
+# # Check if writer is not None before trying to release it
+# if writer is not None:
+#     writer.release()
 
-        # Check if the recording duration is reached
-        if (current_time - start_time) >= record_duration:
-            print(f"Saved video {j}")
-            writer.release()
-            writer = None
-            j += 1
-            start_time = time.time()
-
-    cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    screen_width = 1920  # Replace with your screen resolution
-    screen_height = 1080  # Replace with your screen resolution
-
-    #cv2.resizeWindow('frame', screen_width, screen_height)
-
-    cv2.imshow('frame', frame)
-
-    key = cv2.waitKey(1)
-
-    if key == ord('q'):
-        save = not save
-        if save:
-            print("Started recording")
-
-    elif key == 27:
-        break
-
-# Check if writer is not None before trying to release it
-if writer is not None:
-    writer.release()
-
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
