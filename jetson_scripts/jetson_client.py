8042c35f45c7cac5d275630e6745e2efbf9f98ec
Grzetan <grzetan@gmail.com>
2023-12-21 09:26:45 +0100

    Improved getting detections from roboflow

--- jetson_scripts/jetson_client.py
+++ jetson_scripts/jetson_client.py
@@ -11,18 +11,23 @@ img = cv2.imread("ok.png")
 start = time.time()
 _, encoded = cv2.imencode(".png", img)
 base64_image = base64.b64encode(encoded.tobytes()).decode('utf-8')
-cv2.imwrite("/tmp/detection.png", img)
 
 data = {"timestamp": 12345, "latitude": 90, "longitude": 12, "image": base64_image}
 
+# Send detection to server
 res = requests.post("http://127.0.0.1:3000/detection", json=data)
-print(res)
+print(res.text)
 
-out = subprocess.check_output(f"base64 /tmp/detection.png | curl -d @- \"http://127.0.0.1:9001/pbl-2023/2?api_key=kY5y1URLQQGG8D9Fp5LP\"", shell=True, text=True)
+# Get detections from roboflow model
+
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+res = requests.post("http://127.0.0.1:9001/pbl-2023/2?api_key=kY5y1URLQQGG8D9Fp5LP", data=base64_image, headers=headers)
+print(res.text)
+
+# out = subprocess.check_output(f"base64 /tmp/detection.png | curl -d @- \"http://127.0.0.1:9001/pbl-2023/2?api_key=kY5y1URLQQGG8D9Fp5LP\"", shell=True, text=True)
 end = time.time()
 print(end - start)
 
-print(out)
 # cap = cv2.VideoCapture(0)
 
 # width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
